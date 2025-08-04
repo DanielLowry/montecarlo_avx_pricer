a1b8c44ccde5a704210aaf7ec1257c6fdd669630
Dan <danlowry1991@googlemail.com>
2025-08-04 18:44:37 -0400

    Formatting only

--- analytics/hull_white_1f.cpp
+++ analytics/hull_white_1f.cpp
@@ -1,8 +1,9 @@
 
 #include "hull_white_1f.hpp"
-#include "logging.hpp"
 
 #include <random>
+
+#include "logging.hpp"
 
 hull_white_1f::hull_white_1f(const std::shared_ptr<const discount_curve>& curve, double a,
                              double sigma)
@@ -33,14 +34,11 @@ inline date_t operator-(date_t t, double dt) { return t + (-dt); }
 
 double hull_white_1f::price_cap_monte_carlo(date_t start_date, date_t end_date, double strike,
                                             double notional, int num_paths) const {
-
-
     double dcf = (end_date - start_date) / 365.0;  // start date only needed to calculate the DCF
     double df = curve_->df(end_date);
 
     date_t val_date = curve_->valuation_date();
-    if (end_date <= val_date) 
-        throw std::runtime_error("end_date must be after valuation_date");    
+    if (end_date <= val_date) throw std::runtime_error("end_date must be after valuation_date");
 
     size_t num_steps = end_date - val_date;
 
@@ -48,8 +46,7 @@ double hull_white_1f::price_cap_monte_carlo(date_t start_date, date_t end_date, 
     for (int i = 0; i < num_paths; ++i) {
         // Run the monte carlo simulation
         double r = curve_->fwd(val_date, val_date + (end_date - start_date));
-        for (size_t j = 0; j < num_steps; ++j) 
-            r = evolve(val_date + j, r, 1);
+        for (size_t j = 0; j < num_steps; ++j) r = evolve(val_date + j, r, 1);
 
         // Caplet payoff
         sum_payoffs += std::max(r - strike, 0.0);
@@ -60,8 +57,6 @@ double hull_white_1f::price_cap_monte_carlo(date_t start_date, date_t end_date, 
 
 double hull_white_1f::price_cap_black(date_t start_date, date_t end_date, double strike,
                                       double notional) const {
-
-
     // Simple Black-Scholes formula for an IR caplet
     double fwd = curve_->fwd(start_date, end_date);
 
@@ -94,21 +89,18 @@ double hull_white_1f::evolve(date_t ti,        // Current time
     // It's a reasonably complicated derivation, which leads to the following formula:
     // theta(t) = dfwd(0,t)/dt + a * fwd(0,t) + sigma^2 / (2 * a) * (1 - exp(-2 * a * t))
     // We calculate dfwd(0,t)/dt by finite difference
-    constexpr double epsilon = 1;  // for finite difference 
+    constexpr double epsilon = 1;  // for finite difference
     double dfwd_dt =
         (curve_->fwd(ti + epsilon, ti + 2 * epsilon) - curve_->fwd(ti, ti + epsilon)) / epsilon;
-    double theta =
-        dfwd_dt + a_ * curve_->fwd(ti, ti + epsilon) +
-        sigma_ * sigma_ / (2.0 * a_) * (1.0 - std::exp(-2.0 * a_ * date_to_double(ti)));
+    double theta = dfwd_dt + a_ * curve_->fwd(ti, ti + epsilon) +
+                   sigma_ * sigma_ / (2.0 * a_) * (1.0 - std::exp(-2.0 * a_ * date_to_double(ti)));
 
     // Standard 1F hull white
     double rn = random_normal();
     return ri + (theta - a_ * ri) * dt + sigma_ * std::sqrt(dt) * rn;
 }
 
-
 double hull_white_1f::black_vol_to_hw_vol(double black_vol, double a, double dcf, double T) {
     return black_vol * std::sqrt((2 * a * T) / (1 - std::exp(-2 * a * T))) * (a * dcf) /
            (1 - std::exp(-a * dcf));
 }
-
